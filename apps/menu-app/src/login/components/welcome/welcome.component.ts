d7ca6bf7add3398766ab02622019830b2cd55ac7
Pablo Antipan <antipan.pablo@gmail.com>
2024-12-28 20:20:02 -0300

    [ADD] welcome component

--- apps/menu-app/src/login/components/welcome/welcome.component.ts
+++ apps/menu-app/src/login/components/welcome/welcome.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./welcome.component.scss'],
 })
 export class WelcomeComponent {
+  userData = {
+    names: 'John Doe',
+  };
   constructor() { }
 }
