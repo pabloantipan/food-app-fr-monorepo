8415a067fd2b0cac4f5c5a3d77d0c92225ce0e3a
Pablo Antipan <antipan.pablo@gmail.com>
2024-12-28 22:40:22 -0300

    [ADD] sign up component

--- apps/menu-app/src/login/login.page.ts
+++ apps/menu-app/src/login/login.page.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { SignInComponent } from './components/sign-in/sign-in.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
 import { WelcomeComponent } from './components/welcome/welcome.component';
 
 @Component({
   standalone: true,
   imports: [
     SignInComponent,
+    SignUpComponent,
     WelcomeComponent,
   ],
   selector: 'app-login-page',
